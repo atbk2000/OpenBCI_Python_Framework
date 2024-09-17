c2a7db5c54cbc85ab90b75539186cf3ef0bd640a
atbk2000 <antoniothommaz207@gmail.com>
2024-09-17 20:32:31 -0300

    Renomeando DataReplicate para Interpolate
    Renomeando nó DataReplicate para Interpolate.

--- models/node/processing/interpolate.py
+++ models/node/processing/interpolate.py
@@ -6,24 +6,24 @@ from models.framework_data import FrameworkData
 from models.node.processing.processing_node import ProcessingNode
 
 
-class DataReplicate(ProcessingNode):
-    """ This node is used to replicate the data in the input buffer by a certain number.
+class Interpolate(ProcessingNode):
+    """ This node is used to replicate the data in the input buffer.
 
     Attributes:
         _MODULE_NAME (str): The name of the module (in this case ``models.node.
-        processing.data_replicate``)
+        processing.interpolate``)
         INPUT_MAIN (str): The name of the main input (in this case ``main``) 
         OUTPUT_MAIN (str): The name of the main output (in this case ``main``)
 
     configuration.json usage:
         **module** (*str*): The name of the module (``models.node.processing``)\n
-        **type** (*str*): The name of the class (``DataReplicate``)\n
-        **data_repetition_count** (*int*): Number of times that the data in the input buffer must be repeated.\n
+        **type** (*str*): The name of the class (``Interpolate``)\n
+        **repetition_count** (*int*): Number of times that the data in the input buffer must be repeated.\n
         **use_extra_data** (*bool*): Whether to use extra data when replicating.\n
-        **extra_data** (*int*): Additional data. Simply multiplies the last data in the input by a certain number. Must be smaller than ``data_repetition_count``. Only mandatory if ``use_extra_data`` is True.\n 
+        **extra_data** (*int*): Additional data. Simply multiplies the last data in the input by a certain number. Must be smaller than ``repetition_count``. Only mandatory if ``use_extra_data`` is True.\n 
     """
 
-    _MODULE_NAME: Final[str] = 'node.processing.dataReplicate'
+    _MODULE_NAME: Final[str] = 'node.processing.interpolate'
 
     INPUT_MAIN: Final[str] = 'main'
     OUTPUT_MAIN: Final[str] = 'main'
@@ -34,19 +34,19 @@ class DataReplicate(ProcessingNode):
         :param parameters: The parameters passed to the node.
         :type parameters: dict
 
-        :raises MissingParameterError: The ``data_repetition_count`` parameter is required. The ``extra_data`` parameter is required if ``use_extra_data`` is True.
-        :raises InvalidParameterValue: The ``data_repetition_count`` and ``extra_data`` parameters must be integers. The ``use_extra_data`` parameter must be bool. 
-        :raises InvalidParameterValue: The ``data_repetition_count`` parameter must be bigger than 1. The ``extra_data`` parameter must be bigger than 1 and smaller than ``data_repetition_count``.
+        :raises MissingParameterError: The ``repetition_count`` parameter is required. The ``extra_data`` parameter is required if ``use_extra_data`` is True.
+        :raises InvalidParameterValue: The ``repetition_count`` and ``extra_data`` parameters must be integers. The ``use_extra_data`` parameter must be bool. 
+        :raises InvalidParameterValue: The ``repetition_count`` parameter must be bigger than 1. The ``extra_data`` parameter must be bigger than 1 and smaller than ``repetition_count``.
         """
 
-        if 'data_repetition_count' not in parameters:
-            raise MissingParameterError(module=self._MODULE_NAME, name=self.name, parameter='data_repetition_count')
+        if 'repetition_count' not in parameters:
+            raise MissingParameterError(module=self._MODULE_NAME, name=self.name, parameter='repetition_count')
         
-        if type(parameters['data_repetition_count']) is not int:
-            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='data_repetition_count', cause='must_be_int')
+        if type(parameters['repetition_count']) is not int:
+            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='repetition_count', cause='must_be_int')
         
-        if parameters['data_repetition_count'] <= 1:
-            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='data_repetition_count', cause='must_be_bigger_than_1')
+        if parameters['repetition_count'] <= 1:
+            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='repetition_count', cause='must_be_bigger_than_1')
         
         if 'use_extra_data' not in parameters:
             raise MissingParameterError(module=self._MODULE_NAME, name=self.name, parameter='use_extra_data')
@@ -61,17 +61,17 @@ class DataReplicate(ProcessingNode):
             if type(parameters['extra_data']) is not int:
                 raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='extra_data', cause='must_be_int')
             
-            if (parameters['extra_data'] <= 1) or (parameters['extra_data'] >= parameters['data_repetition_count']):
+            if (parameters['extra_data'] <= 1) or (parameters['extra_data'] >= parameters['repetition_count']):
                 raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name, parameter='extra_data', cause='invalid_value')
         
     def _initialize_parameter_fields(self, parameters: dict):
-        """ Initialize the parameter fields of the node. This method will set the ``data_repetition_count`` and ``use_extra_data`` attributes and all the parent attributes as well. It sets also ``extra_data`` if ``use_extra_data`` is True.
+        """ Initialize the parameter fields of the node. This method will set the ``repetition_count`` and ``use_extra_data`` attributes and all the parent attributes as well. It sets also ``extra_data`` if ``use_extra_data`` is True.
 
         :param parameters: The parameters passed to the node.
         :type parameters: dict
         """
         super()._initialize_parameter_fields(parameters)
-        self.data_repetition_count = parameters['data_repetition_count']
+        self.repetition_count = parameters['repetition_count']
         self.use_extra_data = parameters['use_extra_data']
         if self.use_extra_data == True:
             self.extra_data = parameters['extra_data']
@@ -108,7 +108,7 @@ class DataReplicate(ProcessingNode):
             channel_elements = data[self.INPUT_MAIN].get_data_on_channel(channel)
 
             for element in channel_elements:
-                processed_data.input_data_on_channel([element for _ in range(0,self.data_repetition_count)], channel)
+                processed_data.input_data_on_channel([element for _ in range(0,self.repetition_count)], channel)
 
             if self.use_extra_data == True:
                 processed_data.input_data_on_channel([channel_elements[-1] for _ in range(0, self.extra_data)], channel)
